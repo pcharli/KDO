7e0c801f3e1b108ce09b004a8b2b7e8219879e1a
James Boydens <james.boydens@gmail.com>
2025-11-24 11:19:45 +0100

    update header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="min-h-screen bg-gray-100">
     <header class="bg-white shadow p-4 flex justify-between items-center">
+      <div class="left">
       <h1 class="text-xl font-bold">🎁 Wishlist Familiale</h1>
-     <div v-if="userStore.currentUser">
-  <span class="mr-4 font-medium">Bonjour, {{ userStore.currentUser.name }}</span>
-  <button @click="logout" class="bg-red-500 text-white px-3 py-1 rounded">
-    Déconnexion
-  </button>
-</div>
+      <div v-if="userStore.currentUser">
+        <span class="mr-4 font-medium">Bonjour, {{ userStore.currentUser.name }}</span>
+        <button @click="logout" class="bg-red-500 text-white px-3 py-1 rounded">
+          Déconnexion
+        </button>
+      </div>
+      </div>
 
-<nav class="flex gap-4" v-if="userStore.currentUser">
-  <router-link to="/">Les souhaits</router-link>
-  <router-link to="/reserved">Mes réservations</router-link>
-</nav>
+      <nav class="flex gap-4" v-if="userStore.currentUser">
+        <router-link to="/">Souhaits</router-link>
+        <router-link to="/reserved">Réservations</router-link>
+      </nav>
     </header>
 
     <main class="p-4 max-w-4xl mx-auto">
